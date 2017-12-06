b8bc1e61782a13451a8a666262e8ffb40269bb99
Weronika Felczak <weronika.dominiak@hotmail.com>
2017-12-06 11:36:49 +0100

    Changed styles

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -70,7 +70,7 @@ const appRoutes: Routes = [
     AboutUsComponent,
     RegulationsComponent,
     LoginComponent,
-    OfferDetailComponent
+    OfferDetailComponent,
   ],
   imports: [
     BrowserModule,
