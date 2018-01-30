9ab6d00734f82ff5cee02728893bac2bf0e5fb1e
Weronika <weronika.dominiak@hotmail.com>
2018-01-30 23:19:45 +0100

    Return all

--- backend/apps/volontulo/models.py
+++ backend/apps/volontulo/models.py
@@ -69,9 +69,7 @@ class OffersManager(models.Manager):
 
     def get_for_administrator(self):
         """Return all offers for administrator to allow management."""
-        return self.filter(
-            offer_status__in=('unpublished', 'published', 'rejected')
-        ).all()
+        return self.all()
 
     def get_weightened(self):
         """Return all published offers ordered by weight."""
